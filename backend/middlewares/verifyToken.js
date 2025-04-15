38a58c4f20d5e13e3cf7a23e28146ca4a712469e
kaneki-212903 <132456512+kaneki-212903@users.noreply.github.com>
2025-04-16 00:47:49 +0530

    Create verifyToken.js

---
+++ backend/middlewares/verifyToken.js
@@ -0,0 +1,10 @@
+const verifyToken = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) return res.status(403).json("No token");
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.status(403).json("Invalid token");
+    req.user = user;
+    next();
+  });
+};
