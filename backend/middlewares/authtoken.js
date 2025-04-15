1afa8156f165fd10b808c47cb1eefc18c771937e
kaneki-212903 <132456512+kaneki-212903@users.noreply.github.com>
2025-04-16 00:46:58 +0530

    Update authtoken.js

--- backend/middlewares/authtoken.js
+++ backend/middlewares/authtoken.js
@@ -23,5 +23,16 @@ const authtoken = (req, res, next) => {
         res.status(401).json({ message: 'Unauthorized - Invalid token' });
     }
 };
+const verifyToken = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) return res.status(403).json("No token");
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.status(403).json("Invalid token");
+    req.user = user;
+    next();
+  });
+};
+
 
 module.exports = authtoken;
